634daa1ecbffc652287c600565cec01c94431f7a
Agustina Belén Fernández <agustinabelenfernandez05@gmail.com>
2024-10-09 10:20:39 -0300

    ADD: Defino cargar pacientes

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ def mostrar_opciones():
         10. Salir
         """
     )
+
+def cargar_pacientes(lista_pacientes: list) -> list:
+    """ 
+    Carga a un nuevo paciente con los datos ingresados por el usuario
+    
+    Args:
+        lista_pacientes(list): la lista de pacientes a agregar uno nuevo
+    
+    Return:
+        (list): La lista de pacientes actuallizada con el nuevo paciente
+    """
+    num_de_historia_clinica = int(input("Ingrese el número de historia clinica: "))
+    nombre_paciente = str(input("Ingrese el nombre del paciente: ")).capitalize()
+    edad_paciente = int(input("Ingrese la edad del paciente: "))
+    diagnostico = str(input("Ingrese el diagnostico del paciente: ")).capitalize()
+    cantidad_dias_internacion = int(input("Ingrese la cantidad de dias de internacion: "))
+    
+    datos_nuevo_paciente = [num_de_historia_clinica, nombre_paciente, edad_paciente, diagnostico, cantidad_dias_internacion]
+    
+    lista_pacientes.append(datos_nuevo_paciente)
+    
+    return lista_pacientes
