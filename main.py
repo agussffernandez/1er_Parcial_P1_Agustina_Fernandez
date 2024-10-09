4aa5790b823205a22a69e0af06e8ed2218159758
Agustina Belén Fernández <agustinabelenfernandez05@gmail.com>
2024-10-09 10:06:24 -0300

    ADD: Defino mostrar opciones del menu

---
+++ main.py
@@ -0,0 +1,17 @@
+def mostrar_opciones():
+    """ 
+    Muestra las opciones disponibles
+    """
+    print(
+        """ 
+        1. Cargar pacientes
+        2. Mostrar la lista de pacientes
+        3. Busqueda de pacientes
+        5. Ordenamiento de pacientes
+        6. Determinar el paciente con más dias de internación
+        7. Determinar el paciente con menos dias de internación
+        8. Cantidad de pacientes con días de internación mayor a 5 días
+        9. Promedio de días de internación de todos los pacientes
+        10. Salir
+        """
+    )
